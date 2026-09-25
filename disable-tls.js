c563662e7354edb1b0198e1eccb2898020bf3629
itDager1 <240528251+itDager1@users.noreply.github.com>
2026-09-25 12:36:49 +0200

    Keep TLS verification on unless explicitly disabled

--- disable-tls.js
+++ disable-tls.js
@@ -1,4 +1,6 @@
+import 'dotenv/config';
 import https from 'https';
 
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
-https.globalAgent.options.rejectUnauthorized = false;
+if (process.env.NODE_TLS_REJECT_UNAUTHORIZED === '0') {
+  https.globalAgent.options.rejectUnauthorized = false;
+}
